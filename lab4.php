4a3a3f318fdaa5b6eb7345680a12ba3bc6fa7fad
Shamond Haynes <hayness4@winthrop.edu>
2016-10-31 11:54:35 -0400

    lab4

--- lab4.php
+++ lab4.php
@@ -7,15 +7,12 @@ if($_SERVER["REQUEST_METHOD"] == "GET")
 <!DOCTYPE html>
 <html> 
     <head>
-        Times Tables
-    </head>
-    <title>
-    	Lab 4
-    </title>
+		<title>Times Tables</title>
+	</head>
     <body> 
         <form method="POST" action="lab4.php">
-        	<label> Start: <input type="text" name="start"/></label>
-            <label> End: <input type="text" name="end"/> </label><br><br>
+        	<label> Start: <input type="number" name="start"/></label>
+            <label> End: <input type="number" name="end"/> </label><br><br>
             <label><input type="submit" name="submit" value="Submit" /></label>
         </form>
     </body>
@@ -28,38 +25,48 @@ if($_SERVER["REQUEST_METHOD"] == "GET")
 <!DOCTYPE html>
 <html> 
     <head>
-        Times Tables
+        <title>Times Tables</title>
     </head>
-    <title>
-    	Lab 4
-    </title>
     <body> 
 	<?php 
 			
 		$start = $_POST["start"];
 		$end = $_POST["end"];
 					
-		echo "<table><tr><td>";
-		for($col = $start; $col <= $end; $col++)
- 	  	{
-		    echo "<th>" .  $col . "</th>";
-	    }
-		
-		echo "<tr>";
-	    
-		for($row = $start; $row <= $end; $row++)
+		if(is_numeric($start) && is_numeric($end))
 		{
-			echo "<th>" . $row . "</th>";
-			
+			echo "<table><tr><td>";
 			for($col = $start; $col <= $end; $col++)
-    		{
-				echo "<td>" . ($row * $col) . "</td>";
-	   		}
+			{
+				echo "<th>" .  $col . "</th>";
+			}
+		
+			echo "<tr>";
+	    
+			for($row = $start; $row <= $end; $row++)
+			{
+				echo "<th>" . $row . "</th>";
+				
+				for($col = $start; $col <= $end; $col++)
+				{
+					echo "<td>" . ($row * $col) . "</td>";
+				}
 						
-			echo "</td></tr>";
+				echo "</td></tr>";
+			}
+			echo "</table>";
+			echo "</body></html>";
 		}
-		echo "</table>";
-		echo "</body></html>";
+		
+		if($start < 0 && $end < 0 && $end != $start)
+		{
+			echo "Go back and change your numbers!";
+		}
+		
+		if(is_nan($start) && is_nan($end))
+		{
+			echo "Go back and put a number!";
+		}
 	?>
 <?php
 }
