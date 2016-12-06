8458ae114250965ec08f20c069c1bdf2a261a1d7
Shamond Haynes <hayness4@winthrop.edu>
2016-12-05 22:52:35 -0500

    lab 8

--- subscribe.php
+++ subscribe.php
@@ -5,13 +5,13 @@ require_once("header.php");
 $to = $_POST["email"];
 $from = "no-reply@winthrop.edu";
 $headers = "From:" . $from . "\r\n";
-$subject = "Subscribe";
-$body = "Welcome to the site!";
+$subject = "Subscription";
+$body = "Welcome to the site" . $_POST['name'];
 if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) )
 { 
-    if (mail($to, $subject, $body, $headers, "-f " . $from))
+    if (mail($to, $subject, $body, $headers))
     {
-        echo 'Your e-mail (' . $_POST['email'] . ') has been added to our mailing list!';
+        echo $_POST['name'] . ', your e-mail (' . $_POST['email'] . ') has been added to our mailing list!';
     }
     else
     {
@@ -23,8 +23,9 @@ else
    echo 'There was a problem with your e-mail (' . $_POST['email'] . ')';   
 }
 ?>
+
+<br><a href="http://deltona.birdnest.org/~acc.hayness4/F16-CSCI241/index.php">Home</a>
+
 <?php
 require("footer.php");
 ?>
-
-<br><a href="http://deltona.birdnest.org/~acc.hayness4/F16-CSCI241/Lab%207/index.php">Home</a>
--- subscription.php
+++ subscription.php
@@ -9,7 +9,7 @@ require_once("header.php");
 	<body>
 		<h3>You have reached the maximum number of times to look at an article.<br>
 		In order for you to keep looking at them you will have to subscribe<br>
-		to the site and with the subscription, you will be able to look at<br>
+		to the site. With the subscription, you will be able to look at<br>
 		more news articles than what you have seen recently.</h3>
 		<h1>Subscribe:</h1>
 		<form method="post" action="subscribe.php">
@@ -17,8 +17,7 @@ require_once("header.php");
 			Email: <input type="text" class="email" name="email" size="25" required>
 			<input type="submit" value="submit" name="submit">
 		</form>
+		<br><a href="index.php">Home</a>
 <?php
 require("footer.php");
 ?>
-
-<br><a href="index.php">Home</a>
